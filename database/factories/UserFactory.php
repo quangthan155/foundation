5796a520d51507d60c9a10d74a8c00886e2371c2
QTA <quangthan155@gmail.com>
2019-03-27 14:09:26 +0700

    Laravel only

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(User::class, function (Faker $faker) {
+    return[
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'facebook_id' => Str::random(100),
+        'phone' => rand(1111111111,9999999999),
+        'email_verified_at' => now(),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => Str::random(10),
+    ];
+});
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {   
+        factory('App\User', 10)->create();
+    }
+}
